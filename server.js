0754dc8e54803b0c630bab68b7e529e242106d5e
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-06-16 18:46:47 -0300

    res status y body

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var cors = require("cors")
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 var router = express.Router()
 var app = express()
 // parse application/x-www-form-urlencoded========
@@ -14,21 +14,31 @@ app.use(cors())
 app.use(router)
 const port = 3000
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.header({
-    "header-server": "Genial",
+    'header-server': 'Genial',
   })
   console.log(req.headers) //para ver el header de la peticion
-  res.send("hola")
+  res.send('hola')
 })
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   console.log(req.body)
   console.log(req.query)
-  res.send("Post")
+  //res.status(201).send('Elemento creado!')
+  res.status(201).send([
+    {
+      id: 1,
+      name: 'Gustavo',
+    },
+    {
+      job: true,
+      wife: true,
+    },
+  ])
 })
 /* 
 app.get("/", (req, res) => res.send("Hello World!"))*/
 app.listen(port, () =>
-  console.log(`Example app listening on port port! http://localhost:${port}`)
+  console.log(`Esta aplicacion esta escuchando! http://localhost:${port}`)
 )
