0c3614795c6faea8c59ea48bfc6401b62ba8f35b
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-06-16 20:31:31 -0300

    Aplicando ruteo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
-const router = require('./components/message/network')
+const router = require('./network/routes')
 
 var app = express()
 // parse application/x-www-form-urlencoded========
@@ -10,10 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //parse application/json=====================
 app.use(bodyParser.json())
 app.use(cors())
-app.use(router)
+//app.use(router)
 app.use('/app', express.static('public'))
 const port = 3000
 
+router(app)
 app.listen(port, () =>
   console.log(`Esta aplicacion esta escuchando! http://localhost:${port}`)
 )
