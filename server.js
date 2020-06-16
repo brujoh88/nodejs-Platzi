d7d09736274107add32eeec904265af8c59174f7
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-06-16 09:58:03 -0300

    Manejo con el req Body y query

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express")
-const app = express()
+const bodyParser = require("body-parser")
 const router = express.Router()
-const port = 3000
+const app = express()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(router)
+const port = 3000
 
 router.get("/", (req, res) => {
   res.send("Groso")
 })
 
 router.post("/", (req, res) => {
+  console.log(req.body)
+  console.log(req.query)
   res.send("Post")
 })
 /* 
