39dd37352f5d73adafe86093260b4b41f4141d3f
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-06-16 09:17:49 -0300

    clase 7

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const app = express()
+const router = express.Router()
 const port = 3000
+app.use(router)
 
-app.get("/", (req, res) => res.send("Hello World!"))
+router.get("/", (req, res) => {
+  res.send("Groso")
+})
+
+router.post("/", (req, res) => {
+  res.send("Post")
+})
+/* 
+app.get("/", (req, res) => res.send("Hello World!"))*/
 app.listen(port, () =>
   console.log(`Example app listening on port port! http://localhost:${port}`)
 )
