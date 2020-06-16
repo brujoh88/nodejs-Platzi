75aff58fb43e8b89c58c045c5f4a986e70a249e6
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-06-16 19:40:16 -0300

    Archivo response

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 var router = express.Router()
+const response = require('./network/response')
 var app = express()
 // parse application/x-www-form-urlencoded========
 app.use(bodyParser.urlencoded({ extended: false }))
-
 //parse application/json=====================
 app.use(bodyParser.json())
-
 app.use(cors())
-
 app.use(router)
 const port = 3000
 
@@ -19,14 +17,19 @@ router.get('/', (req, res) => {
     'header-server': 'Genial',
   })
   console.log(req.headers) //para ver el header de la peticion
-  res.send('hola')
+  response.success(req, res, 'Mensaje personalizado', 200)
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'Ups fallo', 400)
+  } else {
+    response.success(req, res, 'Mensaje Creado', 201)
+  }
+  /*console.log(req.body)
   console.log(req.query)
-  //res.status(201).send('Elemento creado!')
-  res.status(201).send([
+  res.status(201).send('Elemento creado!')
+   res.status(201).send([
     {
       id: 1,
       name: 'Gustavo',
@@ -35,7 +38,7 @@ router.post('/', (req, res) => {
       job: true,
       wife: true,
     },
-  ])
+  ]) */
 })
 /* 
 app.get("/", (req, res) => res.send("Hello World!"))*/
